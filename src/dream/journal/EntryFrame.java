73993ff7b19d9e6da66591c74b8fde2dbc9c7d0e
avivzohman2 <zohman@chapman.edu>
2021-11-15 12:33:03 -0800

    New frames and buttons

--- src/dream/journal/EntryFrame.java
+++ src/dream/journal/EntryFrame.java
@@ -9,12 +9,12 @@ package dream.journal;
  *
  * @author avivzohman
  */
-public class WelcomeFrame extends javax.swing.JFrame {
+public class EntryFrame extends javax.swing.JFrame {
 
     /**
      * Creates new form WelcomeFrame
      */
-    public WelcomeFrame() {
+    public EntryFrame() {
         initComponents();
     }
 
@@ -34,6 +34,7 @@ public class WelcomeFrame extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         textAreaEntry = new javax.swing.JTextArea();
         javax.swing.JButton btnClear = new javax.swing.JButton();
+        backButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Dream");
@@ -66,6 +67,13 @@ public class WelcomeFrame extends javax.swing.JFrame {
             }
         });
 
+        backButton.setText("Back");
+        backButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                backButtonActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -86,19 +94,25 @@ public class WelcomeFrame extends javax.swing.JFrame {
                         .addComponent(btnClear)
                         .addGap(133, 133, 133)))
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(backButton)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
+                .addComponent(backButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(labelTitle)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(18, 18, 18)
                 .addComponent(tfTitle, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(27, 27, 27)
+                .addGap(18, 18, 18)
                 .addComponent(labelEntry)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 74, Short.MAX_VALUE)
+                .addGap(34, 34, 34)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnOK)
                     .addComponent(btnClear))
@@ -116,7 +130,13 @@ public class WelcomeFrame extends javax.swing.JFrame {
 
     private void btnOKActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOKActionPerformed
         String title = tfTitle.getText();
-        labelTitle.setText(title);  
+        if (title.isEmpty()) { //FIXME
+            labelTitle.setText("A Majestic Journey");
+        }
+        else {
+            labelTitle.setText(title);  
+        }
+ 
         dreamTitles[0] = title; 
         dreamEntries[0] = textAreaEntry.getText();
         System.out.println(dreamTitles[0]);
@@ -126,6 +146,12 @@ public class WelcomeFrame extends javax.swing.JFrame {
     private void tfTitleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tfTitleActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_tfTitleActionPerformed
+
+    private void backButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backButtonActionPerformed
+        IntroFrame introFrame = new IntroFrame(); 
+        introFrame.setVisible(true); //open IntroFrame
+        setVisible(false); //close current frame
+    }//GEN-LAST:event_backButtonActionPerformed
 
     
     
@@ -144,28 +170,30 @@ public class WelcomeFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(WelcomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EntryFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(WelcomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EntryFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(WelcomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EntryFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(WelcomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EntryFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new WelcomeFrame().setVisible(true);
+                new EntryFrame().setVisible(true);
             }
         });
     }
     
-    private String[] dreamTitles = new String[10];
-    private String[] dreamEntries = new String[10]; 
+    public String[] dreamTitles = new String[10];
+    public String[] dreamEntries = new String[10]; 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton backButton;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel labelTitle;
     private javax.swing.JTextArea textAreaEntry;
